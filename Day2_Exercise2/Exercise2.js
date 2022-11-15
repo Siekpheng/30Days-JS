26d515744e09c6ed2e4853ad5767a75e6724978f
Siekpheng <msiekpheng@gmail.com>
2022-11-15 21:30:53 +0700

    #

---
+++ Day2_Exercise2/Exercise2.js
@@ -0,0 +1,22 @@
+//1
+console.log("There is no exercise better for the heart than reaching down and lifting people up.");
+//2
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
+//3
+let num1 = '10';
+console.log(typeof(num1));
+let number = Number(num1);
+console.log(typeof(number));
+//4
+let num2 = '9.8';
+console.log(parseFloat(num2));
+console.log(Math.ceil(num2));
+//5
+let string1 = "python";
+console.log(string1.includes("on"));
+let string2 = "jargon";
+console.log(string2.includes("on"));
+//6
+let sentence = "I hope this course is not full of jargon.";
+console.log(sentence.includes("jargon"));
+//7
