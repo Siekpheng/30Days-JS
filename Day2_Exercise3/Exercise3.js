cd659f9c24a1554b4fcadc87163203905bda078a
Siekpheng <msiekpheng@gmail.com>
2022-11-16 12:21:14 +0700

    #

--- Day2_Exercise3/Exercise3.js
+++ Day2_Exercise3/Exercise3.js
@@ -8,7 +8,7 @@ let pattern2 = /because/gi;
 console.log((sentence2.match(pattern2)).length);
 //3
 const sentence3 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
-let regEx = /[!@#$%&]/g;
+let regEx = /[!@#$%&;]/g;
 let cleanSentence3 = sentence3.replace(regEx, "");
 let arraySentence3 = cleanSentence3.split(" ");
 let mf = 1;
