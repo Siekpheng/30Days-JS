09879e49b97d5fb86ae3de7765e83d6f9413fc34
Siekpheng <msiekpheng@gmail.com>
2022-11-16 11:07:36 +0700

    #

--- Day2_Exercise2/Exercise2.js
+++ Day2_Exercise2/Exercise2.js
@@ -33,4 +33,4 @@ console.log(string3.charAt(random_number));
 console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
 //12
 let sentence2 = "You cannot end a sentence with because because because is a conjunction";
-console.log(sentence2.substring(31, 54));
+console.log(sentence2.substr(31, 23));
