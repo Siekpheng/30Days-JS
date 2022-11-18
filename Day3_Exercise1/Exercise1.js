e88396f4cd77bdd9eab4f702a547d68cabdf3fa8
Siekpheng <msiekpheng@gmail.com>
2022-11-18 09:02:03 +0700

    #

---
+++ Day3_Exercise1/Exercise1.js
@@ -0,0 +1,26 @@
+//1
+const firstName = "Siekpheng";
+const lastName = "Meng";
+const country = "Cambodia";
+const city = "Phnom Penh";
+let age = 19;
+let isMarried = false;
+let year = 2022;
+console.log(typeof(firstName), typeof(lastName), typeof(country), typeof(city), typeof(age), typeof(isMarried), typeof(year));
+//2
+console.log(typeof("10") == typeof(10));
+//3
+console.log(parseInt("9.8") == 10);
+//4
+let true1 = 3<4, ture2 = 10>5, true3 = 100<199;
+let false1 = 4<3, false2 = 10<5, false3 = 199<100;
+//5
+console.log(
+    4 > 3,
+    4 >= 3,
+    4 < 3,
+    4 <= 3,
+    4 == 4,
+    4 === 4,
+    
+)
